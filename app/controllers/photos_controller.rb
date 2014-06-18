9e3b38dbe279ca780587b9dc22c444e9566c2a25
Seven90 <stevesurya@gmail.com>
2014-06-17 22:45:59 -0400

    Styled and delete and edit finished

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,20 +1,44 @@
 class PhotosController < ApplicationController
   def new
+    @photo = Photo.new
   end
 
   def create
     @photo = Photo.new(photo_params)
 
-    @photo.save 
+    if @photo.save 
     redirect_to @photo
+  else 
+    render 'new'
+  end
   end
 
   def show
-    @photos = Photo.all
+    @photo = Photo.find(params[:id])
   end
 
   def index
     @photos = Photo.all
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    if @photo.update(photo_params)
+        redirect_to @photo
+    else 
+      render 'edit'
+    end
+  end
+
+  def edit 
+    @photo = Photo.find(params[:id])
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_path
   end
 
   private
